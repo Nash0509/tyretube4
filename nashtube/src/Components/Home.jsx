5c2286ca5c3638259806bcdebee11558447d2267
unknown <nishantsinghworkshard@gmail.com>
2023-08-21 17:42:30 +0530

    Final commit

--- nashtube/src/Components/Home.jsx
+++ nashtube/src/Components/Home.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import './Home.css'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
@@ -6,11 +7,49 @@ import {faYoutube, faTiktok} from '@fortawesome/free-brands-svg-icons';
 
 const Home = () => {
   const [videos, setVideos] = useState([]);
-  const [views, setView] = useState([]);
+  const [views, setView] = useState(false);
   const [loading, setLoading] = useState('Loading...');
   const [count, setCount] = useState(1);
   const [search, setSearch] = useState('');
   const [term, setterm] = useState('new videos')
+
+
+   
+  function handlenav() {
+    setView(true)
+    if(views) {
+      document.getElementsByClassName('side')[0].style.width = '50px'
+      document.getElementsByClassName('side')[0].style.backgroundColor= 'black'
+      document.getElementsByClassName('home1')[0].style.fontSize = '12px'
+      document.getElementsByClassName('home2')[0].style.fontSize = '12px'
+      document.getElementsByClassName('home3')[0].style.fontSize = '12px'
+      document.getElementsByClassName('library')[0].style.fontSize = '12px'
+      document.getElementsByClassName('history')[0].style.fontSize = '12px'
+      document.getElementsByClassName('your')[0].style.fontSize = '12px'
+      document.getElementsByClassName('wlater')[0].style.fontSize = '12px'
+      document.getElementsByClassName('download')[0].style.fontSize = '12px'
+      document.getElementsByClassName('subs')[0].style.fontSize = '12px'
+      document.getElementsByClassName('c1')[0].style.fontSize = '12px'
+      document.getElementsByClassName('c2')[0].style.fontSize = '12px'
+      document.getElementsByClassName('c3')[0].style.fontSize = '12px'
+      document.getElementsByClassName('c4')[0].style.fontSize = '12px'
+      document.getElementsByClassName('c5')[0].style.fontSize = '12px'
+      document.getElementsByClassName('more')[0].style.gridTemplateColumns = '1fr 1fr 1fr 1fr'
+      document.getElementsByClassName('more')[0].style.paddingLeft = '9.5vw'
+      document.getElementsByClassName('more')[0].style.width = '92vw'
+
+      setView(false);
+    }
+    else {
+      document.getElementsByClassName('side')[0].style.width = '13.1vw'
+      document.getElementsByClassName('more')[0].style.gridTemplateColumns = '1fr 1fr 1fr'
+      document.getElementsByClassName('more')[0].style.paddingLeft = '16.5vw'
+  
+      setView(true);
+    }
+
+   
+  }
 
   function handleChange(e) {
     setSearch(e.target.value)
@@ -23,7 +62,7 @@ const Home = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const key = 'AIzaSyBH4PN4GiTtqyN_i4KpWU_eiwVBbKF9LkM';
+        const key = 'AIzaSyBEj9uDjtNOjQeyG7Nps3vOUO9raqAQsHU';
         const apiUrl = `https://www.googleapis.com/youtube/v3/search?key=${key}&part=snippet&q=${term}&maxResults=20000`;
 
         const response = await fetch(apiUrl);
@@ -97,7 +136,7 @@ function calculateTimeDifference(publishTime) {
     <div>
       <div className="top" style={{display:'grid', gridTemplateColumns:'0.267fr 0.467fr 0.267fr'}}>
         <h1 className="title" style={{color:'white', boxSizing:'border-box', paddingRight:'4rem', fontFamily:'Roboto, sans-serif'}}>
-         <FontAwesomeIcon icon={faBars} style={{fontSize:'23px', paddingRight:'1rem'}}/>&nbsp; <FontAwesomeIcon icon={faYoutube} style={{color:'red'}}/>  Tyre<span style={{color:'red'}}>Tube</span></h1>
+         <FontAwesomeIcon icon={faBars} className='threedots'  style={{fontSize:'23px', paddingRight:'1rem'}} onClick={handlenav}/>&nbsp; <FontAwesomeIcon icon={faYoutube} style={{color:'red'}}/>  Tyre<span style={{color:'red'}}>Tube</span></h1>
         <div style={{display:'flex', alignItems:'center'}}>
         <input type="text" className='bars'  placeholder='search...' name='search' onChange={handleChange}/>
       <button onClick={startSearch}><FontAwesomeIcon icon={faMagnifyingGlass}  style={{color:'white'}}/></button>
@@ -106,30 +145,29 @@ function calculateTimeDifference(publishTime) {
      
       </div>
 
-     <nav className="side" style={{height:'100vh', width:'13.1vw', position:'fixed'}}>
-      <h5 className="home1"><FontAwesomeIcon icon={faHome}/> Home</h5>
-      <h5 className="home2"><FontAwesomeIcon icon={faTiktok}/> Shorts</h5>
-      <h5 className="home3"><FontAwesomeIcon icon={faShieldVirus}/> Subcriptions</h5>
-      <hr style={{color:'silver', width:'10vw', textAlign:'center', marginLeft:'1.55vw'}}/>
+      <nav className="side" style={{height:'100vh', width:'13.1vw', position:'fixed'}}>
+      <h5 className="home1"><FontAwesomeIcon icon={faHome} style={{textAlign:'center'}}/> <span className='und1'>Home</span></h5>
+      <h5 className="home2"><FontAwesomeIcon icon={faTiktok} style={{textAlign:'center'}}/> Shorts</h5>
+      <h5 className="home3"><FontAwesomeIcon icon={faShieldVirus} style={{textAlign:'center'}}/> Subcriptions</h5>
+      <hr  style={(views) ? {width:'10vw',color:'silver', textAlign:'center', marginLeft:'1.55vw'}:{width:'0vw',color:'silver', textAlign:'center', marginLeft:'0vw'}}/>
       <h5 style={{ color:'silver'}}/>
-      <h5 className="library"><FontAwesomeIcon icon={faBook}/> Library</h5>
-      <h5 className="history"><FontAwesomeIcon icon={faHistory}/> History</h5>
+      <h5 className="library"><FontAwesomeIcon icon={faBook} style={{textAlign:'center'}}/> Library</h5>
+      <h5 className="history"><FontAwesomeIcon icon={faHistory} style={{textAlign:'center'}}/> History</h5>
       <h5 className="your"><FontAwesomeIcon icon={faVideo}/> Your videos</h5>
-      <h5 className="wlater"><FontAwesomeIcon icon={faTimeline}/> Watch Later</h5>
-      <h5 className="download"><FontAwesomeIcon icon={faDownload}/> Downloads</h5>
-      <hr style={{ color: 'silver', width:'10vw', marginLeft:'1.55vw'}}/>
-      <h5 className="subs"><FontAwesomeIcon icon={faStream}/> Subscriptions</h5>
-      <h5 className="c1">Republic Bharat <FontAwesomeIcon icon={faTowerBroadcast} style={{color:'orange'}}/></h5>
-      <h5 className="c2">Zee News <FontAwesomeIcon icon={faTowerBroadcast} style={{color:'orange'}}/></h5>
-      <h5 className="c3">The Lallantop <FontAwesomeIcon icon={faTowerBroadcast} style={{color:'orange'}}/></h5>
-      <h5 className="c4">MrBeast <FontAwesomeIcon icon={faTowerBroadcast} style={{color:'orange'}}/></h5>
-      <h5 className="c5">Cartoon Network <FontAwesomeIcon icon={faTowerBroadcast} style={{color:'orange'}}/></h5>3
+      <h5 className="wlater"><FontAwesomeIcon icon={faTimeline} style={{textAlign:'center'}}/> Watch Later</h5>
+      <h5 className="download"><FontAwesomeIcon icon={faDownload} style={{textAlign:'center'}}/> Downloads</h5>
+      <hr style={(views) ? {width:'10vw',color:'silver', textAlign:'center', marginLeft:'1.55vw'}:{width:'0vw',color:'silver', textAlign:'center', marginLeft:'0vw'}}/>
+      <h5 className="subs"><FontAwesomeIcon icon={faStream} style={{textAlign:'center'}}/> Subscriptions</h5>
+      <h5 className="c1">Republic Bharat <FontAwesomeIcon icon={faTowerBroadcast} style={{color:'orange', textAlign:'center'}}/></h5>
+      <h5 className="c2">Zee News <FontAwesomeIcon icon={faTowerBroadcast} style={{color:'orange', textAlign:'center'}}/></h5>
+      <h5 className="c3">The Lallantop <FontAwesomeIcon icon={faTowerBroadcast} style={{color:'orange', textAlign:'center'}}/></h5>
+      <h5 className="c4">MrBeast <FontAwesomeIcon icon={faTowerBroadcast} style={{color:'orange', textAlign:'center'}}/></h5>
+      <h5 className="c5">Cartoon Network <FontAwesomeIcon icon={faTowerBroadcast} style={{color:'orange', textAlign:'center'}}/></h5>3
      </nav>
-     
-      <div className="more">
+      <div className="more" style={(views) ? {width:'78vw'}:{width:'86vw'}}>
         {videos.slice(0, count * 10).map((video, index) => (
           <div className='card'  key={index}>
-            <img  src={video.snippet.thumbnails.medium.url} alt="image" style={{width:'27vw', borderRadius:'10px'}}/>
+            <img  src={video.snippet.thumbnails.medium.url} alt="image" style={(views) ? {width:'25vw', borderRadius:'10px'}:{width:'21vw', borderRadius:'10px'}}/>
             <div className="desti">
               <h4 style={{color:'white', fontFamily:'Roboto, sans-serif', marginLeft:'1rem'}}>{video.snippet.title}</h4>
               <h5 style={{ marginLeft: '1rem' , fontFamily:'Roboto, sans-serif', color:'grey', marginTop:'1rem'}}>{video.snippet.channelTitle}</h5>
